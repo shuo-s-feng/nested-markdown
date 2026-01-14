c5646ce95b34f160aa3d2dde53e803011b2ab185
shuo-s-feng <shuo.s.feng@gmail.com>
2026-01-14 16:29:59 +0800

    fix: Fix broken import from process.default.cwd and release v0.0.5

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
+  platform: "browser",
   dts: true,
   clean: true,
   sourcemap: true,
